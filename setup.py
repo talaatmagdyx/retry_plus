a69b1464d341106f2efbe880badf121c92a558e9
talaatmagdyx <talaatmagdy75@gmail.com>
2024-06-04 05:00:53 +0300

    added readme as description

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='retry_plus',
-    version='1.0.0',
+    version='1.0.1',
     description='A generic retry package for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Talaat Magdy',
     author_email='talaatmagdy75@gmail.com',
     packages=find_packages(),
-    license="MIT",
     install_requires=[
         'pytest',
         'pytest-asyncio',
     ],
-    test_suite='tests',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
